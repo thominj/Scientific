44dc4481f28fce225881accad69db3098d587c06
Michael Cordingley <Michael.Cordingley@gmail.com>
2014-04-03 15:24:20 -0400

    Stubbing out the tests for special functions.

---
+++ tests/mcordingley/math/SpecialFunctionsTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace mcordingley\math;
+
+class SpecialFunctionsTest extends PHPUnit_Framework_TestCase {
+	public function test_erf() {
+	}
+
+	public function test_ierf() {
+	}
+
+	public function test_gamma() {
+	}
+
+	public function test_gammaln() {
+	}
+
+	public function test_igamma() {
+	}
+
+	public function test_digamma() {
+	}
+
+	public function test_lambert() {
+	}
+
+	public function test_lowerGamma() {
+	}
+	
+	public function test_ilowerGamma() {
+	}
+	
+	public function test_upperGamma() {
+	}
+
+	public function test_beta() {
+	}
+
+	public function test_regularizedIncompleteBeta() {
+	}
+
+	public function test_iregularizedIncompleteBeta() {
+	}
+}
