b252096bfcbfc0b84f4ace75d495a288c9d7cd24
Michael Cordingley <Michael.Cordingley@gmail.com>
2014-04-19 20:03:50 -0400

    Place-holders for missing tests. Just statically fail.

--- tests/mcordingley/Scientific/NumericalTest.php
+++ tests/mcordingley/Scientific/NumericalTest.php
@@ -4,41 +4,54 @@ namespace mcordingley\Scientific;
 
 class NumericalTest extends \PHPUnit_Framework_TestCase {
 	public function test_erf() {
+        $this->fail('Test not implemented.');
 	}
 
 	public function test_ierf() {
+        $this->fail('Test not implemented.');
 	}
 
 	public function test_gamma() {
+        $this->fail('Test not implemented.');
 	}
 
 	public function test_gammaln() {
+        $this->fail('Test not implemented.');
 	}
 
 	public function test_igamma() {
+        $this->fail('Test not implemented.');
 	}
 
 	public function test_digamma() {
+        $this->fail('Test not implemented.');
 	}
 
 	public function test_lambert() {
+        $this->fail('Test not implemented.');
 	}
 
 	public function test_lowerGamma() {
+        $this->fail('Test not implemented.');
 	}
 	
 	public function test_ilowerGamma() {
+        $this->fail('Test not implemented.');
 	}
 	
 	public function test_upperGamma() {
+        $this->fail('Test not implemented.');
 	}
 
 	public function test_beta() {
+        $this->fail('Test not implemented.');
 	}
 
 	public function test_regularizedIncompleteBeta() {
+        $this->fail('Test not implemented.');
 	}
 
 	public function test_iregularizedIncompleteBeta() {
+        $this->fail('Test not implemented.');
 	}
 }
